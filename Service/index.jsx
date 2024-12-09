d8698a9c4a1c74fa67955e478c16102c7018b470
mickbuck <mb2756@byu.edu>
2024-12-09 11:58:39 -0700

    service commit 4

--- Service/index.jsx
+++
@@ -1,6 +0,0 @@
-/* This needs to be able to communicate with PAckage.json and be in the same spot */import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './src/App';
-// why is this broken?
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
--- conflictTest.md
+++
@@ -1,4 +0,0 @@
-This is a conflict test for the Github assignment. It seems like it worked and is functioning properly.
-I am now on GitHub editing this and it looks good!
-This is another change from Github!
-
